6ba21e1c78c3879f043c571f98e92ab52abfbced
bachtiarnaomi <99959074+bachtiarnaomi@users.noreply.github.com>
2023-04-10 23:32:27 +0800

    fixed form logo. deployment ready

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
           href="https://forms.gle/kW5AfW9ZqT3tfVMZA"
           target="_blank"
         >
-          <img src="images/logo-google-form.jpg" alt=""></img>
+          <img src="images/logo-google-square.png" alt=""></img>
           <p className="bold">Google Form</p>
           <p>
             Submit a feedback, report an issue or indicate your interest to
